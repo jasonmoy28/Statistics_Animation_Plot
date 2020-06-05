389162557220d6c16b3b21bd92ab9c73e628a52c
Jason Moy <57694545+jasonmoy28@users.noreply.github.com>
2020-06-05 11:57:13 -0700

    Create saveGIF_simple.R

---
+++ saveGIF_simple.R
@@ -0,0 +1,9 @@
+saveGIF_simple <- function(loop_object,movie_name,loop_number = 1,loop_speed = 0.1) {
+  # movie_name do not want full path, name only 
+  # loop_number (default 1) specify number of loop you want to see for the gif 
+  # loop_speed (default 0.1) is the number of frame per seconds
+  library('animation')
+  library('tidyverse')
+  savedPath = paste('/Users/Jasonmoy/Desktop/Hattrup Lab/Statistics_Animation_Plot/Generated Graph',movie_name)
+  saveGIF({loop_object}, movie.name = savedPath,... = ani.options(interval = loop_speed, loop = loop_number))
+}
