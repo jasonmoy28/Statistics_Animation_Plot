633577644f045bfd229569568e944ec23a9bc060
Jason Moy <57694545+jasonmoy28@users.noreply.github.com>
2020-06-05 11:08:04 -0700

    added plot 2

---
+++ Generating Code/Plot2_Code.R
@@ -0,0 +1,13 @@
+x = seq(0,20,length = 1000)
+saveGIF({for (df in seq(3,15,.5)) {
+  # Edit generation of data here 
+  y = dchisq(x,df)
+  mean = mean(y)
+  median = median(y)
+  plot1_data_in_loop = as.data.frame(cbind(x,y,mean,median,index))
+  # Edit ggplot here 
+  plot_ = ggplot(data = plot1_data_in_loop,aes(x = x,y = y)) + 
+    lims(x = c(0,20), y = c(0,0.25)) + 
+    geom_line()
+  print(plot(plot_))
+}},... = ani.options(interval = .1))
