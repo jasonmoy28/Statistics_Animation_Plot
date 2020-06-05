0fa60382a7adb52cf3ff1a16021dfe86e7e729ca
Jason Moy <57694545+jasonmoy28@users.noreply.github.com>
2020-06-05 12:18:21 -0700

    change names

--- generated_code/Plot1_code.R
+++ generated_code/Plot1_code.R
@@ -1,16 +1,15 @@
-source('~/Desktop/Hattrup Lab/Statistics_Animation_Plot/saveGIF_simple.R')
-x = seq(-5,5,0.15)
-saveGIF_simple(
-for (sd in seq(1,5,.05)) {
-  y = dnorm(x,mean = 0,sd = sd)
-  plot2_data_in_loop = as.data.frame(cbind(x, y,sd))
-  plot_ = plot2_data_in_loop %>% 
-    ggplot(data =.,aes(x = x,y = y)) + 
-    geom_line() + 
-    labs(title = paste('Standard Deviation: ',sd))
+source('~/Desktop/Hattrup Lab/Statistics_Animation_Plot/saveGIF_simple.R') #call the function saveGIF_simple
+
+x = seq(0,20,length = 1000)
+saveGIF({for (df in seq(3,15,.5)) {
+  # Edit generation of data here 
+  y = dchisq(x,df)
+  mean = mean(y)
+  median = median(y)
+  plot1_data_in_loop = as.data.frame(cbind(x,y,mean,median,index))
+  # Edit ggplot here 
+  plot_ = ggplot(data = plot1_data_in_loop,aes(x = x,y = y)) + 
+    lims(x = c(0,20), y = c(0,0.25)) + 
+    geom_line()
   print(plot(plot_))
-}, movie_name = 'Plot1.gif')
-
-
-
-  
+}},... = ani.options(interval = .1))
