6164e46de5b959cbbdcd41c169ef542110ef384c
Jason Moy <57694545+jasonmoy28@users.noreply.github.com>
2020-06-05 12:29:37 -0700

    adopt new change name in files

--- generated_code/Plot2_Code.R
+++ generated_code/Plot2_Code.R
@@ -1,7 +1,8 @@
 source('~/Desktop/Hattrup Lab/Statistics_Animation_Plot/saveGIF_simple.R')
 
 x = seq(-5,5,0.15)
-saveGIF_simple({for (sd in seq(1,,.05)) {
+saveGIF_simple(
+  loop_object = for (sd in seq(1,3.5,.05)) {
   y = dnorm(x,mean = 0,sd = sd)
   plot2_data_in_loop = as.data.frame(cbind(x, y,sd))
   plot_ = plot2_data_in_loop %>% 
@@ -9,8 +10,8 @@ saveGIF_simple({for (sd in seq(1,,.05)) {
     geom_line() + 
     labs(title = paste('Standard Deviation: ',sd)) + 
     lims(y = c(0,0.5))
-  print(plot(plot_))
-}}, movie_name = 'Plot2.gif',loop_number = 1,loop_speed = 0.01)
+  print(plot(plot_))}
+  , movie_name = 'Plot2.gif')
 
 
 
--- saveGIF_simple.R
+++ saveGIF_simple.R
@@ -1,4 +1,4 @@
-saveGIF_simple <- function(loop_object,movie_name,loop_number = 1,loop_speed = 0.01) {
+saveGIF_simple <- function(loop_object,movie_name,loop_number = 1,loop_speed = 0.1) {
   # movie_name do not want full path, name only 
   # loop_number (default 1) specify number of loop you want to see for the gif 
   # loop_speed (default 0.1) is the number of frame per seconds
