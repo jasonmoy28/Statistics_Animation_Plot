ad13ac8475f3b7f82f98881439d6976110640b3e
Jason Moy <57694545+jasonmoy28@users.noreply.github.com>
2020-06-05 12:27:05 -0700

    minor changes

--- generated_code/Plot2_Code.R
+++ generated_code/Plot2_Code.R
@@ -1,15 +1,16 @@
 source('~/Desktop/Hattrup Lab/Statistics_Animation_Plot/saveGIF_simple.R')
+
 x = seq(-5,5,0.15)
-saveGIF_simple(
-for (sd in seq(1,5,.05)) {
+saveGIF_simple({for (sd in seq(1,,.05)) {
   y = dnorm(x,mean = 0,sd = sd)
   plot2_data_in_loop = as.data.frame(cbind(x, y,sd))
   plot_ = plot2_data_in_loop %>% 
     ggplot(data =.,aes(x = x,y = y)) + 
     geom_line() + 
-    labs(title = paste('Standard Deviation: ',sd))
+    labs(title = paste('Standard Deviation: ',sd)) + 
+    lims(y = c(0,0.5))
   print(plot(plot_))
-}, movie_name = 'Plot1.gif')
+}}, movie_name = 'Plot2.gif',loop_number = 1,loop_speed = 0.01)
 
 
 
