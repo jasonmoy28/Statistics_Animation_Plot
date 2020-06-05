69a15b4d5d2fdbd2e7c1faf813d596047727be07
Jason Moy <57694545+jasonmoy28@users.noreply.github.com>
2020-06-05 13:19:14 -0700

    added packages latex2exp

--- Required_packages.R
+++ Required_packages.R
@@ -2,4 +2,4 @@
 install.packages('animation')
 install.packages('tidyverse') #for ggplot2
 install.packages('magick')
-
+install.packages("latex2exp")
--- saveGIF_simple.R
+++ saveGIF_simple.R
@@ -4,6 +4,7 @@ saveGIF_simple <- function(loop_object,movie_name,loop_number = 1,loop_speed = 0
   # loop_speed (default 0.1) is the number of frame per seconds
   library('animation')
   library('tidyverse')
+  library('latex2exp')
   savedPath = paste('/Users/Jasonmoy/Desktop/Hattrup Lab/Statistics_Animation_Plot/generated_Graph/',
                     movie_name)
   saveGIF({loop_object}, movie.name = savedPath,... = ani.options(interval = loop_speed, loop = loop_number))
