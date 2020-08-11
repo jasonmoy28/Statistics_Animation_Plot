9ac870c27d4fd4f49b586016542128981697560e
Jason Moy <jasonmoy28@gmail.com>
2020-08-11 06:41:40 +0000

    load package at source instead of function

--- saveGIF_simple_git_ver.R
+++ saveGIF_simple_git_ver.R
@@ -1,12 +1,13 @@
+library('animation')
+library('tidyverse')
+library('latex2exp')
+library('glue')
+
 saveGIF_simple <- function(loop_object,clone_repository_path,movie_name, loop_number = 1,loop_speed = 0.4) {
   # stored path expect path to store (e.g. /Users/Desktop/Folder)
   # movie_name exepect name (e.g. plot.gif)
   # loop_number (default 1) specify number of loop you want to see for the gif 
   # loop_speed (default 0.1) is the number of frame per seconds
-  library('animation')
-  library('tidyverse')
-  library('latex2exp')
-  library('glue')
   save_path = paste(clone_repository_path,'/generated_graph/',movie_name,sep = '')
   saveGIF({loop_object}, movie.name = save_path,... = ani.options(interval = loop_speed, loop = loop_number))
 }
