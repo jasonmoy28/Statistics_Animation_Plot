50cbc304795a65c5b06a97b9fc02d03d9753666f
Jason Moy <jasonmoy28@gmail.com>
2020-08-11 06:57:48 +0000

    edit generated graph

--- saveGIF_simple_git_ver.R
+++ saveGIF_simple_git_ver.R
@@ -8,7 +8,7 @@ saveGIF_simple <- function(loop_object,clone_repository_path,movie_name, loop_nu
   # movie_name exepect name (e.g. plot.gif)
   # loop_number (default 1) specify number of loop you want to see for the gif 
   # loop_speed (default 0.1) is the number of frame per seconds
-  save_path = paste(clone_repository_path,'/generated_graph/',movie_name,sep = '')
+  save_path = paste(clone_repository_path,'generated_graph/',movie_name,sep = '')
   saveGIF({loop_object}, movie.name = save_path,... = ani.options(interval = loop_speed, loop = loop_number))
 }
 
