9636b39ec5e162c44e9d8923e92b7d3ace58d7f4
jasonmoy28 <57694545+jasonmoy28@users.noreply.github.com>
2020-08-11 00:19:09 -0700

    minor changes

--- saveGIF_simple_git_ver.R
+++ saveGIF_simple_git_ver.R
@@ -3,14 +3,15 @@ library('tidyverse')
 library('latex2exp')
 library('glue')
 
-saveGIF_simple <- function(loop_object,clone_repository_path,movie_name, loop_number = 1,loop_speed = 0.4) {
+saveGIF_simple <- function(loop_object,clone_repository_path,movie_name, loop_number = 1,loop_speed = 0.4,path_debugger = F) {
   # stored path expect path to store (e.g. /Users/Desktop/Folder)
   # movie_name exepect name (e.g. plot.gif)
   # loop_number (default 1) specify number of loop you want to see for the gif 
   # loop_speed (default 0.1) is the number of frame per seconds
   save_path = paste('generated_graph/',movie_name,sep = '')
+  print(save_path)
   saveGIF({loop_object}, movie.name = file.path(save_path),... = ani.options(interval = loop_speed, loop = loop_number))
-}
+  }
 
 round_simple <- function(var, number_of_decimals) {
   format(round(var, digits = number_of_decimals),nsmall = number_of_decimals)
